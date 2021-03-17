cab3684440a20a54adf56cd24f70efd89ebf6bba
Kiev Markean Cang <cangkievmarkean@gmail.com>
2021-03-17 23:32:49 +0800

    added a simple widget and kv file
    edited original main.py file to have the prototype of the user class and its functions
    added multiple parts to the new kv file.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 
-class MyApp(App):
-    
+class BisprendEngine(Widget):
+    pass
+
+class BisprendApp(App):
     def build(self):
-        return Label(text='Hello Worlds 2.0')
+        return BisprendEngine()
 
+class User:
+    def __init__(self):
+        self.__name = ""
+        self.__progress = 0
+
+    def registername(self, newname):
+        self.__name = newname
+
+    def updateuserprogress(self, newprog:int):
+        self.__progress = newprog
 
 if __name__ == '__main__':
-    MyApp().run()
+    BisprendApp().run()
