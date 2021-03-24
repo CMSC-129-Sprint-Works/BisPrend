02b0c834995ee7611acf74ee3538d4828624c408
Kiev Markean Cang <cangkievmarkean@gmail.com>
2021-03-24 20:49:51 +0800

    put together two sets of files into one
    outside files
    main.py - put into the folder
    .kv file - put into the folder
    
    others - deleted

--- BisPrend/main.py
+++ BisPrend/main.py
@@ -13,16 +13,42 @@ from kivy.uix.label import Label
 
 Config.set('graphics', 'resizable', True)
 
+class User:
+    def __init__(self):
+        self.__name = ""
+        self.__progress = 0
+
+    def registername(self, newname):
+        self.__name = newname
+
+    def updateuserprogress(self, newprog:int):
+        self.__progress = newprog
+
+    def getName(self):
+        return self.__name 
+        #im assuming this is for the file saving -kiev
+
+    def getProgress(self):
+        return self.__progress
+
+
 class BisprendEngine(Widget):
+    newPlayer = User()
+    def checkProgress(self):
+        pass
+
+    # called when the button is pressed/released
     def btn(self):
         self.createUserFile()
+        print(f"Name: {self.newPlayer.getName()} \nProgress: {self.newPlayer.getProgress()}")
         popup = BisprendPopup()
         popup.show_popup()
-    
+
     # create user file after registration (or when the "ok"/"confirm" button is pressed/released)
     def createUserFile(self):
         userFile = open("userfile.txt", "w")
-        userFile.write(self.name.text + "\n0")
+        userFile.write(self.player.text + "\n0")
+        self.newPlayer.registername(self.player.text)
         userFile.close()
 
 
@@ -39,18 +65,6 @@ class BisprendApp(App):
         engine = BisprendEngine()
         return engine
 
-
-class User:
-    def __init__(self):
-        self.__name = ""
-        self.__progress = 0
-
-    def registername(self, newname):
-        self.__name = newname
-        print(newname)
-
-    def updateuserprogress(self, newprog:int):
-        self.__progress = newprog
 
 #Registering Font
 LabelBase.register(name="Mont",
