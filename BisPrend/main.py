3593d98664c38cdc73011270f9866b47dd69bf9d
Kiev Markean Cang <cangkievmarkean@gmail.com>
2021-05-21 07:00:58 +0800

    added generalization of word/lesson loading, added information database for active use
     - added generalized global function that creates the carousel only once upon selecting the specific subcategory
     - added global list that helps identify if a carousel has been added to a subcategory already
     - added information database for the function stated above
     - commented out the MDCARDS in the kv file
     - commented out some parts of the QUIZ due to lack of knowledge in collating it together

--- BisPrend/main.py
+++ BisPrend/main.py
@@ -9,20 +9,47 @@ from kivy.config import Config
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.carousel import Carousel
+from kivy.uix.image import Image
+from kivy.graphics.instructions import Canvas
+
 
 #kivymd
 from kivymd.uix.label import MDLabel
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.card import MDCard
+from kivymd.uix.button import MDRectangleFlatButton
 
 from kivy.clock import Clock
 from user import User
-import quiz
-
+import sqlite3
+# import quiz
 
 Config.set('graphics', 'resizable', True)
 
 newPlayer = User() #global scope (for testing)
+
+check = []
+for i in range(0,14):
+    check.append(0)
+
+def CarouselMaker(category:str,subcategory:str):
+    conn = sqlite3.connect('Information.db')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM Information WHERE Category = \"" + category + "\" AND Subcategory = \"" + subcategory +"\"")
+    listofall = curs.fetchall()
+
+    caros = Carousel(direction = "right",size_hint=(1,.91))
+    for i in listofall:
+        imagesrc = i[3]
+        sampbis = i[4]
+        sampeng = i[5]
+        image = Image(source=imagesrc)
+        card = MDCard(size_hint = (.4,.8),pos_hint = {"center_x": .5, "center_y": .5},)
+        card.add_widget(image)
+        caros.add_widget(card)
+
+    return caros
 
 #Screens
 class PageManager(ScreenManager):
@@ -56,59 +83,167 @@ class MenuSelector(Screen):
     def playername(self):
         return newPlayer.getName()
 
-# class BalayPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class PamilyaPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class ResibidorPage (Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class KomidorPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class Kan_ananPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class KatulgananPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class KasilyasPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
+#Balay and subcategories
+class BalayPage(Screen):
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
 
-# class SkuylahanPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class ClassroomPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class NumeroPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class ClinicPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class LibPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
-# class CanteenPage(Screen):
-#     def on_enter(self):
-#         self.manager.updateTracker(self.name)
+class PamilyaPage(Screen):
+    def on_pre_enter(self):
+        if check[0] != 1:
+            caros = CarouselMaker("Balay","Pamilya")
+            self.add_widget(caros)
+            check[0] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+    def on_leave(self):
+        print("yawa")
+
+class ResibidorPage (Screen):
+    def on_pre_enter(self):
+        if check[1] != 1:
+            caros = CarouselMaker("Balay","Resibidor")
+            self.add_widget(caros)
+            check[1] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class KomidorPage(Screen):
+    def on_pre_enter(self):
+        if check[2] != 1:
+            caros = CarouselMaker("Balay","Komidor")
+            self.add_widget(caros)
+            checl[2] = 1
+    
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class Kan_ananPage(Screen):
+    def on_pre_enter(self):
+        if check[3] != 1:
+            caros = CarouselMaker("Balay","Kan-anan")
+            self.add_widget(caros)
+            check[3] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class KatulgananPage(Screen):
+    def on_pre_enter(self):
+        if check[4] != 1:
+            caros = CarouselMaker("Balay","Katulganan")
+            self.add_widget(caros)
+            check[4] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class KasilyasPage(Screen):
+    def on_pre_enter(self):
+        if check[5] != 1:
+            caros = CarouselMaker("Balay","Kasilyas")
+            self.add_widget(caros)
+            check[5] = 1 
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+#skuylahan and subcategories
+
+class SkuylahanPage(Screen):
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class ClassroomPage(Screen):
+    def on_pre_enter(self):
+        if check[6] != 1:
+            caros = CarouselMaker("Skuylahan","Lawak Tunghaan")
+            self.add_widget(caros)
+            check[6] = 1
+    
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class NumeroPage(Screen):
+    def on_pre_enter(self):
+        if check[7] != 1:
+            caros = CarouselMaker("Skuylahan","Numero")
+            self.add_widget(caros)
+            check[7] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class ClinicPage(Screen):
+    def on_pre_enter(self):
+        if check[8] != 1:
+            caros = CarouselMaker("Skuylahan","Tambalan")
+            self.add_widget(caros)
+            check[8] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class LibPage(Screen):
+    def on_pre_enter(self):
+        if check[9] != 1:
+            caros = CarouselMaker("Skuylahan","Bibliyoteka")
+            self.add_widget(caros)
+            check[9] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+class CanteenPage(Screen):
+    def on_pre_enter(self):
+        if check[10] != 1:
+            caros = CarouselMaker("Skuylahan","Kantin")
+            self.add_widget(caros)
+            check[10] = 1
+
+    def on_enter(self):
+        self.manager.updateTracker(self.name)
+
+#tindahan and subcategories
 
 class TindahanPage(Screen):
     def on_enter(self):
         self.manager.updateTracker(self.name)
+
 class PagkaonPage(Screen):
+    def on_pre_enter(self):
+        if check[11] != 1:
+            caros = CarouselMaker("Tindahan","Pagkaon")
+            self.add_widget(caros)
+            check[11] = 1
+
     def on_enter(self):
         self.manager.updateTracker(self.name)
+
 class SaninaPage(Screen):
+    def on_pre_enter(self):
+        if check[12] != 1:
+            caros = CarouselMaker("Tindahan","Sinina")
+            self.add_widget(caros)
+            check[12]
+    
     def on_enter(self):
         self.manager.updateTracker(self.name)
+
 class KwartaPage(Screen):
+    def on_pre_enter(self):
+        if check[13] != 1:
+            caros = CarouselMaker("Tindahan","Kuwarta")
+            self.add_widget(caros)
+            check[13] = 1
+
     def on_enter(self):
         self.manager.updateTracker(self.name)
+
+
 
 class BisprendApp(MDApp):
     def build(self):
